6ec5c036d3838353c73b4d98a296e07f76c1591a
summer0404 <157554348+summer0404@users.noreply.github.com>
2024-03-01 15:59:57 +0700

    Delete Trung-Hạ/Trung/Hạ directory

--- Trung-Hạ/Trung/Hạ/homework_3js.js
+++
@@ -1,8 +0,0 @@
-function getInput(){
-    var input=document.getElementById("txtin").value;
-    if (input == "" || input == null){
-        alert("Please enter a valid resquest.");
-        return;
-    }
-    alert("Your resquest of \"" +input + "\" is being processed.");
-}
